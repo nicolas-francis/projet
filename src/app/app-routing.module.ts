d07f71859ee0cbc417d000c2e95c6ef68bb7ff39
Nicolas-Francis <nicolasgendron@integrationsante.com>
2018-11-22 11:38:48 -0500

    test pour voir si git marche bien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from "./home/home.component";
 import { AddProjectComponent } from "./add-project/add-project.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
     component: HomeComponent
   },
   {
